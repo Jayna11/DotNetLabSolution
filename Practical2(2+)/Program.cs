26c4c0b483709263ae397a32db3663f4c8d08c28
unknown <17.comp.jaina.tanna@gmail.cpm>
2020-02-27 09:21:31 +0530

    new commit

---
+++ Practical2(2+)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Practical2_2__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string country, name;
+
+            do
+            {
+                Console.WriteLine("Enter Your Name : ");
+                name = Console.ReadLine();
+            } while (!Regex.IsMatch(name, @"^[a-zA-Z]+$"));
+
+            do
+            {
+                Console.WriteLine("Enter Your Country : ");
+                country = Console.ReadLine();
+            } while (!Regex.IsMatch(name, @"^[a-zA-Z]+$"));
+
+            Console.WriteLine("Your Name is " + name + " from country " + country);
+            Console.Read();
+        }
+    }
+}
